51e6f6daccd00e68399c9adc807cee944e9ca341
MAC <mac@favour.local>
2025-02-27 21:21:56 +0100

    message

---
+++ index.js
@@ -0,0 +1,41 @@
+// //Variables
+let num1 = "2,4"
+let num2 = "1,3"
+console.log ("Result for num1 variable = ", num1)
+console.log ("Result for num2 variable = ", num2)
+
+// Addition Operation
+function sum(num1, num2){
+ let Result = num1 + num2
+ return Result
+
+}
+console.log("Result for Addition operation = ", sum(35, 10))
+
+// multiplication Operation 
+function multiply(num1, num2){
+    let Result = num1 * num2
+   return Result
+   } 
+  console.log("Result for multiplication operation = ", multiply(35, 10))
+
+  // Subtraction Operation 
+  function Subtract(num1, num2){
+    let Result = num1 - num2
+   return Result
+   }
+  console.log("Result for Subtraction operation = ", Subtract(35, 10))
+
+  // Division Operation
+  function Division(num1, num2){
+    let Result = num1 / num2
+   return Result
+   }
+  console.log("Result for Division operation = ", Division(35, 10))
+
+  // Modulus Operation
+  function Modulus(num1, num2){
+    let Result = num1 % num2
+   return Result
+   }
+  console.log("Result for Modulus operation = ", Modulus(35, 10))
